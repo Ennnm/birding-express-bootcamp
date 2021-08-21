752b59b11765906909ea81a709b3646527aa337f
Ennnm <en@DESKTOP-HB00MK8.localdomain>
2021-08-21 11:31:29 +0800

    listed routes for species

--- init_table.sql
+++ init_table.sql
@@ -10,3 +10,11 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 ALTER TABLE sightings ADD user_id INTEGER;
 
 SELECT * FROM sightings INNER JOIN users  ON sightings.user_id = users.id WHERE users.id=1;
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
+
+INSERT INTO species (name, scientific_name) VALUES ('King Quail', 'Excalfactoria chinensis');
+INSERT INTO species (name, scientific_name) VALUES ('Red Junglefowl', 'Gallus gallus');
+INSERT INTO species (name, scientific_name) VALUES ('Wandering Whistling Duck', 'Dendrocygna arcuata');
+INSERT INTO species (name, scientific_name) VALUES ('Lesser Whistling Duck', 'Dendrocygna javanica');
+INSERT INTO species (name, scientific_name) VALUES ('Cotton Pygmy Goose', 'Nettapus coromandelianus');
