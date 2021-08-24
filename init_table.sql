66a6fdbe9aecfd959f122c2f0a93a864c3a9e150
Ennnm <en@DESKTOP-HB00MK8.localdomain>
2021-08-24 17:23:47 +0800

    update init_table

--- init_table.sql
+++ init_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE sightings (id SERIAL PRIMARY KEY, date DATE, time TIME, behavior TEXT, flock_size INTEGER );
+CREATE TABLE sightings (id SERIAL PRIMARY KEY, date DATE, time TIME, flock_size INTEGER, user_id, species_id );
 
-INSERT INTO sightings (date, time, flock_size) VALUES ('2019-01-01',  '14:00:00', 15 );
-INSERT INTO sightings (date, time, flock_size) VALUES ('2019-03-01',  '15:00:00', 1 );
-INSERT INTO sightings (date, time, flock_size) VALUES ('2020-08-01',  '17:00:00', 4 );
-INSERT INTO sightings (date, time, flock_size) VALUES ('2020-01-01',  '08:00:00', 3 );
+-- INSERT INTO sightings (date, time, flock_size) VALUES ('2019-01-01',  '14:00:00', 15 );
+-- INSERT INTO sightings (date, time, flock_size) VALUES ('2019-03-01',  '15:00:00', 1 );
+-- INSERT INTO sightings (date, time, flock_size) VALUES ('2020-08-01',  '17:00:00', 4 );
+-- INSERT INTO sightings (date, time, flock_size) VALUES ('2020-01-01',  '08:00:00', 3 );
 
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 
-ALTER TABLE sightings ADD user_id INTEGER;
-ALTER TABLE sightings ADD species_id INTEGER;
+-- ALTER TABLE sightings ADD user_id INTEGER;
+-- ALTER TABLE sightings ADD species_id INTEGER;
 
-SELECT * FROM sightings INNER JOIN users  ON sightings.user_id = users.id WHERE users.id=1;
+-- SELECT * FROM sightings INNER JOIN users  ON sightings.user_id = users.id WHERE users.id=1;
 
 CREATE TABLE species (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
 
@@ -30,3 +30,5 @@ INSERT INTO behaviors (behavior) VALUES ('walking'),
                                         ('long song');
 
 CREATE TABLE behavior_sighting (id SERIAL PRIMARY KEY, sight_id INTEGER, behavior_id INTEGER);
+
+CREATE TABLE comments (id SERIAL PRIMARY KEY, sight_id INTEGER, comment TEXT, FOREIGN KEY (sight_id) REFERENCES sightings(id));
