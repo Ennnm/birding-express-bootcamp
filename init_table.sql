2c695c545aec954351dab3594db5642a6d3462cc
Ennnm <en@DESKTOP-HB00MK8.localdomain>
2021-08-25 12:26:52 +0800

    update sql init

--- init_table.sql
+++ init_table.sql
@@ -1,4 +1,12 @@
-CREATE TABLE sightings (id SERIAL PRIMARY KEY, date DATE, time TIME, flock_size INTEGER, user_id INTEGER, species_id INTEGER);
+DROP TABLE IF EXISTS sightings CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS species CASCADE;
+DROP TABLE IF EXISTS behavior CASCADE;
+DROP TABLE IF EXISTS behavior_sighting CASCADE;
+DROP TABLE IF EXISTS comments CASCASE;
+
+
+CREATE TABLE IF NOT EXISTS sightings (id SERIAL PRIMARY KEY, date DATE, time TIME, flock_size INTEGER, user_id INTEGER, species_id INTEGER);
 
 INSERT INTO sightings (date, time, flock_size, user_id, species_id) VALUES ('2019-01-01',  '14:00:00', 15, 1, 1);
 INSERT INTO sightings (date, time, flock_size, user_id, species_id) VALUES ('2019-03-01',  '15:00:00', 1 , 1, 2);
@@ -6,21 +14,17 @@ INSERT INTO sightings (date, time, flock_size, user_id, species_id) VALUES ('202
 INSERT INTO sightings (date, time, flock_size, user_id, species_id) VALUES ('2020-01-01',  '08:00:00', 3 , 1, 4);
 
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+INSERT INTO TABLE (id, username) VALUES (0, 'anon')
 
--- ALTER TABLE sightings ADD user_id INTEGER;
--- ALTER TABLE sightings ADD species_id INTEGER;
+CREATE TABLE species IF NOT EXISTS (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
 
--- SELECT * FROM sightings INNER JOIN users  ON sightings.user_id = users.id WHERE users.id=1;
-
-CREATE TABLE species (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
-
-INSERT INTO species (name, scientific_name) VALUES ('King Quail', 'Excalfactoria chinensis');
+INSERT INTO species  (name, scientific_name) VALUES ('King Quail', 'Excalfactoria chinensis');
 INSERT INTO species (name, scientific_name) VALUES ('Red Junglefowl', 'Gallus gallus');
 INSERT INTO species (name, scientific_name) VALUES ('Wandering Whistling Duck', 'Dendrocygna arcuata');
 INSERT INTO species (name, scientific_name) VALUES ('Lesser Whistling Duck', 'Dendrocygna javanica');
 INSERT INTO species (name, scientific_name) VALUES ('Cotton Pygmy Goose', 'Nettapus coromandelianus');
 
-CREATE TABLE behaviors (id SERIAL PRIMARY KEY, behavior TEXT);
+CREATE TABLE behaviors IF NOT EXISTS (id SERIAL PRIMARY KEY, behavior TEXT);
 
 INSERT INTO behaviors (behavior) VALUES ('walking'),
                                         ('bathing'),
@@ -29,6 +33,8 @@ INSERT INTO behaviors (behavior) VALUES ('walking'),
                                         ('mobbing'),
                                         ('long song');
 
-CREATE TABLE behavior_sighting (id SERIAL PRIMARY KEY, sight_id INTEGER, behavior_id INTEGER);
+CREATE TABLE behavior_sighting IF NOT EXISTS (id SERIAL PRIMARY KEY, sight_id INTEGER, behavior_id INTEGER);
 
-CREATE TABLE comments (id SERIAL PRIMARY KEY, sight_id INTEGER, comment TEXT, commenter_id INTEGER,FOREIGN KEY (sight_id) REFERENCES sightings(id));
+INSERT INTO behavior_sighting (sight_id, behavior_id) VALUES (1,2), (1,5) , (2,6), (2,3), (3,3), (3,4), (4,1);
+
+CREATE TABLE comments IF NOT EXISTS (id SERIAL PRIMARY KEY, sight_id INTEGER, comment TEXT, commenter_id INTEGER,FOREIGN KEY (sight_id) REFERENCES sightings(id));
